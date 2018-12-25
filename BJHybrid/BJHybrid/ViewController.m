58c4d5e0121de2d3b6e51d0df3785798d5b1aab1
Braindie <zhangwenjun0630@163.com>
2018-12-25 18:46:53 +0800

    整理

---
+++ BJHybrid/BJHybrid/ViewController.m
@@ -0,0 +1,102 @@
+//
+//  ViewController.m
+//  BJJSPatch
+//
+//  Created by zhangwenjun on 17/3/7.
+//  Copyright © 2017年 zhangwenjun. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BJWebViewController.h"
+#import "WebViewJavascriptBridgeViewController.h"
+#import "BJWVViewController.h"
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self testAction];
+    
+    self.myTableView.delegate = self;
+    self.myTableView.dataSource = self;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 6;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellId"];
+    cell.textLabel.numberOfLines = 0;
+
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"JSPatch";
+    }else if (indexPath.row == 1){
+        cell.textLabel.text = @"NSURLProtocol";
+
+    }else if (indexPath.row == 2){
+        cell.textLabel.text = @"JavaScriptCore与UIWebView";
+
+    }else if (indexPath.row == 3){
+        cell.textLabel.text = @"WebViewJavascriptBridge";
+
+    }else if (indexPath.row == 4){
+        cell.textLabel.text = @"WKWebView";
+        
+    }else if (indexPath.row == 5){
+        cell.textLabel.text = @"WKWebViewJavascriptBridge";
+        
+    }
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 80;
+}
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (indexPath.row == 0) {
+
+    }else if (indexPath.row == 1){
+        BJWVViewController *vc = [[BJWVViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+
+    }else if (indexPath.row == 2){
+        BJWebViewController *vc = [[BJWebViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+        
+    }else if (indexPath.row == 3){
+        WebViewJavascriptBridgeViewController *vc = [[WebViewJavascriptBridgeViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+        
+    }else if (indexPath.row == 4){
+
+    }else if (indexPath.row == 5){
+
+    }
+}
+
+
+- (void)testAction{
+    NSLog(@"JSPatch");
+//    self.bjLabel.text = @"初始值为0";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # BJJSPatch
 
-###JSPatch
+### JSPatch
 使用JSPatch实现热更新
 
-###NSURLProtocol
+### NSURLProtocol
 通过此协议可以实现webView中的图片用本地资源来代替
 
-###JavaScriptCore
-OC与JS中的方法可以相互调用，不过JS中要有特点的对象作为OC的代理对象
+### JavaScriptCore与UIWebView
+OC与JS中的方法可以相互调用，不过JS中要有特定的对象作为OC的代理对象
 
-###WebViewJavascriptBridge
+### WebViewJavascriptBridge
+只能使用UIWebView<br/>
 通过给JS中注入特点代码，然后匹配方法名可以实现JS与OC的相互调用，并且每次调用都会有回调
+
+### WKWebView
+还没写
+
+### WKWebViewJavascriptBridge
+还没写
